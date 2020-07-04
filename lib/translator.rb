7515ed2074c65d2c237945ed1fb18adba01490b5
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-07-04 05:03:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_english_meaning(file_path, emoticon)
   library.each do |meaning, idiom|
     return meaning if idiom[:japanese] == emoticon
   end
-   
+   "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon(japanese_emoticon)
