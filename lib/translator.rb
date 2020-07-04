b6615ed97313a7ed9f207cbbc0eb7c986e7f7053
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-07-04 02:25:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,14 @@
-
-require 'yaml'
 require 'pry'
+require 'yaml'
 
 def load_library
   
-
-  
-  binding.pry 
-  
-end 
+end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
