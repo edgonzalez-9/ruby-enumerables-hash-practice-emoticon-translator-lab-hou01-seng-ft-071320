7f434c710edbffe320c3b1a41599e667ab789619
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-07-04 04:27:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,9 @@ require 'yaml'
 def load_library(file)
   emoticons = YAML.load_file('lib/emoticons.yml')
   new_hash = {}
-  new_hash = emoticons.transform_keys {
+  new_hash = emoticons.transform_values do |values|
+    {:english, 0}
+  end
   
 end
 
